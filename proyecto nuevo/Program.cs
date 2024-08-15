5a036e9af269002f9ca3d95755064b28abd6f212
Santos-Hernandez29 <sh6074843@gmail.com>
2024-08-15 14:09:44 -0600

    Update Program.cs

--- proyecto nuevo/Program.cs
+++ proyecto nuevo/Program.cs
@@ -1,8 +1,23 @@
-﻿Console.WriteLine("Bienvenidos a nuestro primer sistema creado en C#");
-Console.WriteLine("Ingrese tu nombre para continuar ");
-string Nombre = Console.ReadLine();// Par recibir valores de caracter
-Console.WriteLine("Ingrese su edad");
-int Edad = Convert.ToInt32(Console.ReadLine());//Convertir a dato tipo double
-double EdadMeses = Edad * 12;//Calculos Matematicos
-Console.WriteLine("La edad en Meses es:"+EdadMeses + "Meses");//Concarenacion
-                                                            
+﻿using System;
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Bienvenidos a nuestro primer sistema creado en C#");
+        Console.WriteLine("Ingrese tu nombre para continuar ");
+        string Nombre = Console.ReadLine();// Par recibir valores de caracter
+        Console.WriteLine("Ingrese su edad");
+        int Edad = Convert.ToInt32(Console.ReadLine());//Convertir a dato tipo double
+        double EdadMeses = Edad * 12;//Calculos Matematicos
+        Console.WriteLine("La edad en Meses es:" + EdadMeses + "Meses");//Concarenacion
+        if (Edad > 18)
+        {
+            Console.WriteLine("Usted es Mayor de Edad");
+        }
+        else
+        {
+            Console.WriteLine("Usted no es Mayor de Edad");
+        }
+    }
+}
