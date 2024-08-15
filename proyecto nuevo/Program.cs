91e0553eccd819c49dcbab836411dcdf5341f978
Santos-Hernandez29 <sh6074843@gmail.com>
2024-08-15 13:33:19 -0600

    Update Program.cs

--- proyecto nuevo/Program.cs
+++ proyecto nuevo/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hola, Mundo!");
+﻿Console.WriteLine("Bienvenidos a nuestro primer sistema creado en C#");
+Console.WriteLine("Ingrese tu nombre para continuar ");
+string Nombre = Console.ReadLine();// Par recibir valores de caracter
+Console.WriteLine("Ingrese su edad");
+int Edad = Convert.ToInt32(Console.ReadLine());//Convertir a dato tipo double
+double EdadMeses = Edad * 12;//Calculos Matematicos
+Console.WriteLine("La edad en Meses es:"+EdadMeses + "Meses");//Concarenacion
+                                                            
